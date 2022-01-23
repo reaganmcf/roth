82afb346632f433aab4513f881aa75f694fbaf54
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2022-01-23 13:27:16 -0500

    refactor: use clever macro for op handlers

--- src/op.rs
+++ src/op.rs
@@ -12,54 +12,71 @@ pub enum Op {
     Print,
     Or,
     And,
-    Not
+    Not,
+    Eq,
+}
+
+macro_rules! handlers {
+    ($self:ident, $other:ident, $err:ident, ($t1:ident, $t2:ident, $op:expr)) => {
+        if let Op::$t1 { val: x } = $self {
+            if let Op::$t2 { val: y } = $other {
+                return Ok($op(x,y));
+            } else {
+                return Err(RuntimeError::$err);
+            }
+        }
+    };
+
+    ($self:ident, $other:ident, $err:ident, ($t1:ident, $t2:ident, $op: expr), $(($ot1:ident, $ot2:ident, $oop:expr)),+) => {
+        handlers!($self, $other, $err, ($t1, $t2, $op));
+        handlers!($self, $other, $err, $(($ot1, $ot2, $oop)),+);
+    }
 }
 
 impl Op {
     pub fn add(self, other: Op) -> Result<Op, RuntimeError> {
-        match self {
-            Op::Int { val: y } => match other {
-                Op::Int { val: x } => Ok(Op::Int { val: x + y }),
-                _ => Err(RuntimeError::InvalidAdd),
-            },
-            Op::String { val: y } => match other {
-                Op::String { val: x } => Ok(Op::String {
-                    val: format!("{}{}", x, y),
-                }),
-                _ => Err(RuntimeError::InvalidAdd),
-            },
-            _ => Err(RuntimeError::InvalidAdd),
-        }
+        handlers!(
+            self,
+            other,
+            InvalidAdd,
+            (Int, Int, |x, y| Op::Int { val: x + y }),
+            (String, String, |x, y| Op::String {
+                val: format!("{}{}", x, y)
+            })
+        );
+        Err(RuntimeError::InvalidAdd)
     }
 
     pub fn sub(self, other: Op) -> Result<Op, RuntimeError> {
-        match self {
-            Op::Int { val: y } => match other {
-                Op::Int { val: x } => Ok(Op::Int { val: x - y }),
-                _ => Err(RuntimeError::InvalidSub),
-            },
-            _ => Err(RuntimeError::InvalidSub),
-        }
+        handlers!(
+            self,
+            other,
+            InvalidSub,
+            (Int, Int, |x, y| Op::Int { val: x - y })
+        );
+        Err(RuntimeError::InvalidSub)
     }
 
     pub fn mul(self, other: Op) -> Result<Op, RuntimeError> {
-        match self {
-            Op::Int { val: y } => match other {
-                Op::Int { val: x } => Ok(Op::Int { val: x - y }),
-                _ => Err(RuntimeError::InvalidMul),
-            },
-            _ => Err(RuntimeError::InvalidMul),
-        }
+        handlers!(
+            self,
+            other,
+            InvalidMul,
+            (Int, Int, |x, y| Op::Int { val: x * y })
+        );
+
+        Err(RuntimeError::InvalidMul)
     }
 
     pub fn div(self, other: Op) -> Result<Op, RuntimeError> {
-        match self {
-            Op::Int { val: y } => match other {
-                Op::Int { val: x } => Ok(Op::Int { val: x - y }),
-                _ => Err(RuntimeError::InvalidDiv),
-            },
-            _ => Err(RuntimeError::InvalidDiv),
-        }
+        handlers!(
+            self,
+            other,
+            InvalidDiv,
+            (Int, Int, |x, y| Op::Int { val: x / y })
+        );
+
+        Err(RuntimeError::InvalidDiv)
     }
 
     pub fn print(&self) -> Result<(), RuntimeError> {
@@ -72,29 +89,41 @@ impl Op {
     }
 
     pub fn or(self, other: Op) -> Result<Op, RuntimeError> {
-        match self {
-            Op::Boolean { val: y } => match other {
-                Op::Boolean { val: x } => Ok(Op::Boolean { val: x || y }),
-                _ => Err(RuntimeError::InvalidOr),
-            },
-            _ => Err(RuntimeError::InvalidOr),
-        }
+        handlers!(
+            self,
+            other,
+            InvalidOr,
+            (Boolean, Boolean, |x, y| Op::Boolean { val: x || y })
+        );
+        Err(RuntimeError::InvalidOr)
     }
 
     pub fn and(self, other: Op) -> Result<Op, RuntimeError> {
-        match self {
-            Op::Boolean { val: y } => match other {
-                Op::Boolean { val: x } => Ok(Op::Boolean { val: x && y }),
-                _ => Err(RuntimeError::InvalidAnd),
-            },
-            _ => Err(RuntimeError::InvalidAnd),
-        }
+        handlers!(
+            self,
+            other,
+            InvalidOr,
+            (Boolean, Boolean, |x, y| Op::Boolean { val: x && y })
+        );
+        Err(RuntimeError::InvalidAnd)
     }
 
     pub fn not(self) -> Result<Op, RuntimeError> {
         match self {
             Op::Boolean { val } => Ok(Op::Boolean { val: !val }),
-            _ => Err(RuntimeError::InvalidNot)
+            _ => Err(RuntimeError::InvalidNot),
         }
+    }
+
+    pub fn eq(self, other: Op) -> Result<Op, RuntimeError> {
+        handlers!(
+            self,
+            other,
+            InvalidEq,
+            (Int, Int, |x, y| Op::Boolean { val: x == y }),
+            (String, String, |x, y| Op::Boolean { val: x == y }),
+            (String, String, |x, y| Op::Boolean { val: x == y })
+        );
+        Err(RuntimeError::InvalidEq)
     }
 }
